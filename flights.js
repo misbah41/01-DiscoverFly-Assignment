1935cb650eb47b668af8ab512ef1ffb23da17dcc
hasan <hasanmisbah41@gmail.com>
2021-01-30 17:28:49 +0600

    Ticket counter assignment pushed

--- flights.js
+++ flights.js
@@ -1,69 +1,70 @@
 
+function handleUpdatedNumber(id, isIncrease) {
+	const mainInput = document.getElementById(id);
+	const mainInputValue = parseInt(mainInput.value);
+	let NewCountNumber = mainInputValue;
+	if (isIncrease === true) {
+		NewCountNumber = mainInputValue + 1;
+	}
+	if (isIncrease === false && NewCountNumber > 0) {
+		NewCountNumber = mainInputValue - 1;
+	}
+	mainInput.value = NewCountNumber;
+	totalCalculate();
+};
 
+// function of total value
+function totalCalculate() {
+	const economyCount = getInputvalue('economy-input-count');
+	const firstClassCount = getInputvalue('firstClass-inputCount');
 
+	const subTotal = firstClassCount * 150 + economyCount * 100;
+	const tax = subTotal * 0.1;
+	const totalCost = tax + subTotal;
 
-		const firstClassIncraseBtn = document.getElementById('firstClass-increaseBtn');
-		firstClassIncraseBtn.addEventListener('click', function(){
-			handleUpdatedNumber('firstClass-inputCount', true);			
-		});
+	document.getElementById('sub_total').innerText = '$' + subTotal;
+	document.getElementById('tax').innerText = `$ ${tax}`;
+	document.getElementById('total-cost').innerText = totalCost;
 
-		const firstClassdecraseBtn = document.getElementById('firstClass-decreaseBtn');
-		firstClassdecraseBtn.addEventListener('click', function(){
-			handleUpdatedNumber('firstClass-inputCount', false);
-		});
+};
 
+//function of economy and firstClass input value
+function getInputvalue(id) {
+	const currentInput = document.getElementById(id);
+	const inputCountNumber = parseInt(currentInput.value);
+	return inputCountNumber;
+};
 
-		// economy function
-		const economyIncraseBtn = document.getElementById('economyIncrease-btn');
-		economyIncraseBtn.addEventListener('click', function(){
-			handleUpdatedNumber('economy-input-count', true);
-		});
+// book now button addEventListener function
+const bookNowBtn = document.getElementById('bookNow-btn');
+bookNowBtn.addEventListener('click', function () {
+	const economyCount = getInputvalue('economy-input-count');
+	const firstClassCount = getInputvalue('firstClass-inputCount');
 
-		const economydecraseBtn = document.getElementById('economyDecrease-btn');
-		economydecraseBtn.addEventListener('click', function(){
-			handleUpdatedNumber('economy-input-count', false);
-		});
+	const subTotal = firstClassCount * 150 + economyCount * 100;
+	const tax = subTotal * 0.1;
+	const totalCost = tax + subTotal;
+	console.log(subTotal, tax, totalCost);
 
+	document.getElementById('firstClass-display').innerText = ` First Class Ticket   = ${firstClassCount}`;
+	document.getElementById('economy-display').innerText = ` Economy Class Ticket = ${economyCount}`;
+	document.getElementById('subTotal-display').innerText = ` Subtotal             = ${subTotal}`;
+	document.getElementById('tax-display').innerText = ` Ticket  Tax          = ${tax}`;
+	document.getElementById('total-display').innerText = ` Total Cost           = ${totalCost}`;
 
+	//emty total number Element
+	document.getElementById('sub_total').innerText = '$ 00';
+	document.getElementById('tax').innerText = '$ 00';
+	document.getElementById('total-cost').innerText = '$ 00';
+	//modal style
+	document.getElementById('modal-display').style.display = 'block';
 
-		function handleUpdatedNumber(id, isIncrease){
-			const mainInput = document.getElementById(id);
-			const mainInputValue = parseInt(mainInput.value);
-			// const economyNewCount = economyCount-1;
-			let economyNewCount = mainInputValue;
-			if(isIncrease === true){
-				economyNewCount = mainInputValue + 1;
-			}
-			if(isIncrease === false && economyNewCount>0 ){
-				economyNewCount = mainInputValue - 1;
-			}
-			mainInput.value = economyNewCount;
-			totalCalculate();
-		};
+});
 
+//modal addEventListener;		
+function hiddenModal() {
+	document.getElementById('modal-display').style.display = 'none';
+}
 
-		// function of total value
-		function totalCalculate(){
-			const sub_totalValue = document.getElementById('sub_total').innerText;
-
-			const economyCount = getInputvalue('economy-input-count');
-			const firstClassCount = getInputvalue('firstClass-inputCount');
-
-			const subTotal = firstClassCount*150 + economyCount*100;
-			document.getElementById('sub_total').innerText = '$'+ subTotal;
-			const tax = subTotal*0.1;
-			document.getElementById('tax').innerText = `$ ${ tax}`;
-			document.getElementById('total-cost').innerText =  `$ ${ tax+subTotal}`;
-		};
-		
-		//function of economy and firstClass input value
-		function getInputvalue(id){			
-			const currentInput = document.getElementById(id);
-			const inputCountNumber = parseInt(currentInput.value);
-			return inputCountNumber;
-		};
-
-
-    
 
 
